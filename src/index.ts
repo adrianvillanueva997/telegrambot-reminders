19630f5e774c2bb65cde3be239a632afdf7af4a0
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-07-22 20:32:18 +0200

    added logging

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
+import { Logger } from 'tslog';
 import { check_daily_event } from './specialevents';
 
 const TelegramBot = require('node-telegram-bot-api');
 const cron = require('node-schedule');
 
-let env = process.env['telegram_bot'];
+const env = process.env['telegram_bot'];
 if (env == undefined) {
   console.log("Error: Environment variable 'telegram_bot' not found.");
   process.exit(1);
 }
 
 const bot = new TelegramBot(env, { polling: false });
+const logger = new Logger({ name: 'reminderbot_logger' });
 
-console.log('Bot has been started ...');
+logger.info('Bot has been started ...');
 
 const chatId = -1001063900471;
 
 cron.scheduleJob('0 0 * * *', function () {
+  logger.info('Running special events check ...');
   const event = check_daily_event();
   if (event != '') {
     bot.sendMessage(chatId, event);
@@ -23,5 +26,6 @@ cron.scheduleJob('0 0 * * *', function () {
 });
 
 cron.scheduleJob('0 0 * * 4', function () {
+  logger.info('Sending happy thursday message ...');
   bot.sendMessage(chatId, 'Feliz jueves!');
 });
